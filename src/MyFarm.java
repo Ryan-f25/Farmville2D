81913ff75cd1b57fb8a1d87104ddf2b438938c51
ryanfarokhzad <ryan_farokhzad25@milton.edu>
2023-04-28 12:14:08 -0400

    I tried to do the first problem

--- src/MyFarm.java
+++ src/MyFarm.java
@@ -5,17 +5,28 @@ public class MyFarm {
     }
 
 
+    public Plot[][] grid;
     public MyFarm() {
         System.out.println("hello");
         Plot wick = new Plot();
         wick.printplot();
 
-        Plot [][] grid = new Plot [10][10];
-        for (int p = 0; p <grid.length; p++) {
+        grid = new Plot[4][3];
+        for (int p = 0; p < grid.length; p++) {
             for (int h = 0; h < grid[p].length; h++) {
                 grid[p][h] = new Plot();
                 grid[p][h].printplot();
             }
         }
+        totalplants();
+
     }
+
+    //call methods here
+
+    public void totalplants() {
+        // how many total plants are there in the row?
+        grid.numberofplants();
+    }
+
 }
