6a8f66952a2e1df0492c596d8a8f93e5dedd01a7
ryanfarokhzad <ryan_farokhzad25@milton.edu>
2023-05-16 09:55:24 -0400

    I tried to add more rows as well as removing the second one and printing the amount of elements in the array list.

--- src/MyFarmArrayList.java
+++ src/MyFarmArrayList.java
@@ -2,20 +2,30 @@ import java.util.ArrayList;
 public class MyFarmArrayList {
 
     public ArrayList<Plot> row = new ArrayList<Plot>(); // this is an array list
-    public Plot[] arr = new Plot[10]; // this is an array
+
+
+    //public Plot[] arr = new Plot[10]; // this is an array
 
     public MyFarmArrayList() {
         System.out.println("my farm using array lists");
 
-        arr[0] = new Plot(); // add to an array
+        //arr[0] = new Plot(); // add to an array
         //arr[0].printplot();
 
 
-        for(int p=0; p<10; p++){
+        for (int p = 0; p < 10; p++) {
             System.out.println(row.size());
             row.add(new Plot()); // add to an array list
             row.get(p).printplot();
+            row.add("rice");
+            row.add("meat");
+            row.add("fruits");
+            row.add("veggies");
+            System.out.println(row);
+
         }
-        row.add(new Plot());
+        row.remove(2);
+        System.out.println(row.size());
     }
 }
+
--- src/RunMyFarm.java
+++ src/RunMyFarm.java
@@ -4,4 +4,5 @@ public class RunMyFarm {
 
         MyFarmArrayList wick = new MyFarmArrayList();
     }
+
 }
